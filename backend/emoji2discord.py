04ec978d018df159624362d3c86dd6723be13364
StellarNemesis <stellarnemesis@gmail.com>
2018-03-24 11:36:35 -0400

    Added functionality to emoji2discord.py

--- backend/emoji2discord.py
+++ backend/emoji2discord.py
@@ -1,4 +1,8 @@
-e2d = {
+import re
+import os
+
+# emoji to discord/simplecode
+_e2d = {
     '💥': ':boom:',
     '🛡': ':shield:',
     '🚫': ':no_entry_sign:',
@@ -7,4 +11,80 @@ e2d = {
     '💀': 'skull:',
     '❤': ':heart:',
     '🔷': ':large_blue_diamond:',
+    '🏵️': ':rosette:',
+    '⚔️': ':crossed_swords:',
 }
+
+# discord to emoji
+_d2e = {_e2d[i]: i for i in _e2d}
+
+# discord emojis missing in markdown
+_md = {}
+
+
+def _convert(rules, infile, outfile=None):
+    """Executes a dictionary of replacement rules on a string or file.
+       Arguments:
+       rules   - dictionary of replacement rules.
+       infile  - input file or string
+       outfile - (optional: default None) output filename. If unset return string.
+    """
+    if os.path.isfile(infile):
+        with open(infile, 'r') as f:
+            infile = f.read()
+    for i in rules:
+        infile = re.sub(i, rules[i], infile)
+    if outfile is not None:
+        with open(outfile, 'w') as f:
+            f.write(infile)
+        return None
+    return infile
+
+
+def e2d(infile, outfile=None):
+    """Converts emojis to discord/shortcode.
+           Arguments:
+           infile  - input file or string
+           outfile - (optional: default None) output filename. If unset return string.
+    """
+    return _convert(_e2d, infile, outfile)
+
+
+def d2e(infile, outfile=None):
+    """Converts discord/shortcode to emojis.
+           Arguments:
+           infile  - input file or string
+           outfile - (optional: default None) output filename. If unset return string.
+    """
+    return _convert(_d2e, infile, outfile)
+
+
+def md(infile, outfile=None):
+    """Changes non-markdown discord/shortcode to emojis.
+           Arguments:
+           infile  - input file or string
+           outfile - (optional: default None) output filename. If unset return string.
+    """
+    return _convert(_md, infile, outfile)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Convert emojis in a file.')
+    parser.add_argument('infile', help='input file')
+    parser.add_argument('outfile', default=None, help='Output file')
+    parser.add_argument('-e2d', help='Convert emojis to discord (default)')
+    parser.add_argument('-d2e', help='Convert discord to emoji')
+    parser.add_argument('-md', help='Convert the discord icons not rendered by markdown')
+
+    args = parser.parse_args()
+    if sum([args.e2d, args.d2e, args.md]) > 1:
+        raise RuntimeError('Cannot specify more than 1 conversion option.')
+
+    if args.d2e:
+        print(e2d(args.infile, args.outfile))
+    elif args.md:
+        print(e2d(args.infile, args.outfile))
+    else:
+        print(e2d(args.infile, args.outfile))
